fa7abb0c639b95a5736ac7118f8350bd14a55e00
MatthewJeffson <2768073931@qq.com>
2023-05-25 09:17:57 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -34,7 +34,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'MatthewJeffson', // Usually your GitHub org/user name.
   projectName: 'custom', // Usually your repo name.
-
+  trailingSlash: true,
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
